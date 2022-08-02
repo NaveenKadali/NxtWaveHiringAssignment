622e646ed416ecbb4ffcccf50dd34a67d94355ba
Naveen_Kadali <naveenkadali@outlook.com>
2022-08-02 12:51:32 +0530

    Finalized

--- analyze_quotes.py
+++ analyze_quotes.py
@@ -4,83 +4,68 @@ import sqlite3
 connection = sqlite3.connect('./quote.db')
 cursor = connection.cursor()
 
-# function to retun count of quotes in the table
+# retuns count of quotes in the table
 def get_quotes_count():
     quotes_count_query = """SELECT count(*) FROM quote""" 
     cursor.execute(quotes_count_query) 
-    quotes_count = cursor.fetchone()
-    return quotes_count[0]
+    query_result = cursor.fetchone()
+    return query_result[0]
 
-# function to return no_of_quotes authorised by author
+# returns no_of_quotes authorised by a author
 def get_quotes_count_by_author(author):
     quotes_count_by_author_query="""
-        select 
-            count(*) as count 
-        from 
+        SELECT 
+            COUNT(*) as count 
+        FROM
             quote 
-        where author = ? """
+        WHERE author = ? """
     cursor.execute(quotes_count_by_author_query, (author,))
-    result = cursor.fetchone()
-    return result[0]
+    query_result = cursor.fetchone()
+    return query_result[0]
 
-# function to return min, max and avg tags count 
+# returns a tuple of min, max and avg tags count 
 def get_min_max_avg_tags_count():
     aggrigations_on_tags_query = """
-    select 
-        min(tags_count.count), max(tags_count.count), avg(tags_count.count)
-    from 
-    (
-        select
-            count(*) as count 
-        from 
-            quote_tag 
-        group by 
-            quote_id
-    ) 
-    as tags_count;"""
-
+        SELECT 
+            MIN(tags_count), MAX(tags_count), AVG(tags_count) 
+        FROM (
+            SELECT 
+                count(*) as tags_count 
+            FROM 
+                quote_tag 
+            GROUP BY 
+                quote_id); """
     cursor.execute(aggrigations_on_tags_query);
-    result = cursor.fetchone()
-    return result
+    query_result = cursor.fetchone()
+    return query_result
 
-# function to return t 
-def get_top_n_Authors(N):
-    query = """
-    select
-        author.name, count(*) as no_of_quotes 
-    from
-        author inner join quote on author.name = quote.author 
-    group by
-         author.id 
-    order by 
-        no_of_quotes desc 
-    limit ? ;"""
-    cursor.execute(query,(N,))
-    result = cursor.fetchall()
-    return result
+# returns a list of top n authors
+def get_top_n_authors(n):
+    top_n_authors_query = """
+        SELECT 
+            author 
+        FROM (
+            SELECT 
+                author, count(*) as no_of_quotes 
+            FROM 
+                quote 
+            GROUP BY 
+                author 
+            ORDER BY 
+                no_of_quotes desc
+            ) 
+        LIMIT ?;"""
+    cursor.execute(top_n_authors_query,(n,))
+    query_result = cursor.fetchall()
+    return query_result
 
+# function calls 
+quotes_count = get_quotes_count()
+no_of_quotes_by_author = get_quotes_count_by_author('Albert Einstein')
+min_max_avg_tags = get_min_max_avg_tags_count() 
+top_n_authors_list = get_top_n_authors(5)
 
-# function to call all the functions and to print the result
-def print_responses():
-
-    
-    quotes_count = get_quotes_count()
-    print("\nNumber of quotes on the website are: {}\n".format(quotes_count))
-    
-    # Below print statements is to print data returned get_quotes_count_by_author() function
-    author = 'Albert Einstein'
-    no_of_quotes_by_author = get_quotes_count_by_author(author)
-    print("Number of quotations authored by {} are: {}\n".format(author,no_of_quotes_by_author))
-
-    # Below statements is to print the data returned by min_max_and_avg() function
-    result = get_min_max_avg_tags_count()
-    print("Minimum, Maximum and Average no.of tags: {}, {}, {}\n".format(result[0], result[1], result[2]))
-
-    # Below statemnets are to print the data returned by get_top_n_Authors function.
-    topAuthorsList = get_top_n_Authors(5)
-    for each in topAuthorsList:
-        name = each[0]          # this variable stores author_name
-        no_of_quotes = each[1]  # this variable stores author_no_of_quotes
-        print("Name: {} \tNo_of_quotes: {}".format(name,no_of_quotes))
-
-print_responses() 
+print(quotes_count)
+print(no_of_quotes_by_author)
+print(min_max_avg_tags)
+print(top_n_authors_list)
--- store_quotes.py
+++ store_quotes.py
@@ -1,71 +1,83 @@
 import json
 import sqlite3
-file = open('./quotes.json','r')
 
-quotes_data_dict = json.load(file) 
+# creates a quote tabele to maintain quote and author name
+def create_quote_table():
+    cursor.execute("""DROP TABLE IF EXISTS quote""") # deletes table if exists
+    create_quote_table_query = """
+        CREATE TABLE 
+            quote (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                quote TEXT, author VARCHAR(255)
+            )"""
+    cursor.execute(create_quote_table_query)
+
+# creates an author table in the database to maintain author name, born detials and reference url
+def create_author_table():
+    cursor.execute("""DROP TABLE IF EXISTS author""") # deletes table if exists
+    create_author_table_query = """
+        CREATE TABLE 
+            author (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name VARCHAR(255),
+                born VARCHAR(250),
+                reference VARCHAR(255)
+            )"""
+    cursor.execute(create_author_table_query)
+
+# creates a quote_tag table to maintain quote_id and corresponding tags
+def create_quote_tag_table():
+    cursor.execute("""DROP TABLE IF EXISTS quote_tag""") #deletes table if exists
+    create_quote_tag_table_query = """
+        CREATE TABLE 
+            quote_tag (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                tag VARCHAR(255),
+                quote_id INTEGER NOT NULL, 
+                FOREIGN KEY(quote_id) REFERENCES quote(id)
+            )"""
+    cursor.execute(create_quote_tag_table_query) 
+
+# inserts quote and author name into the quote table
+def insert_into_quote_table():
+    for quote_dict in quotes_data['quotes']:
+        quote = quote_dict['quote']
+        author = quote_dict['author']
+        insert_into_quote_query = """INSERT INTO quote VALUES(?,?,?)"""
+        cursor.execute(insert_into_quote_query,(None,quote,author)) 
+
+# inserts author name, born details and reference url into author table
+def insert_into_author_table():
+    for author_dict in quotes_data['authors']:
+        name = author_dict['name']
+        born = author_dict['born']
+        reference =  author_dict['reference']
+        insert_into_author_query = """INSERT INTO author VALUES (?,?, ?, ?)"""
+        cursor.execute(insert_into_author_query,(None, name, born, reference))
+
+# inserts quote_id and corresponding tags into the quote_tag table
+def insert_into_quote_tag_table():
+    quote_id = 0
+    for quote_dictionary in quotes_data['quotes']:
+        tags_list = quote_dictionary['tags']
+        for tag in tags_list:
+            insert_into_tag_query = """INSERT INTO quote_tag VALUES(?,?,?)"""
+            cursor.execute(insert_into_tag_query,(None, tag,quote_id))
+        quote_id += 1
+
+
+with open('./quotes.json','r') as json_file:
+    quotes_data = json.load(json_file) 
 
 connection = sqlite3.connect('quote.db')
 cursor = connection.cursor()
 
-# this function will delete the existing tables in the database
-def drop_tables():
-    cursor.execute("""DROP TABLE IF EXISTS quote""")
-    cursor.execute("""DROP TABLE IF EXISTS author""")
-    cursor.execute("""DROP TABLE IF EXISTS quote_tag""")
-
-# this function will create tables in the database
-def create_tables():
-    create_quote_table_query = """CREATE TABLE quote (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            quote TEXT, author VARCHAR(255))
-        """
-    create_author_table_query = """CREATE TABLE 
-        author (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name VARCHAR(255),
-            born VARCHAR(250),
-            reference VARCHAR(255)
-            )"""
-    create_quote_tag_table_query = """CREATE TABLE 
-        quote_tag (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            tag VARCHAR(255),
-            quote_id INTEGER, 
-            FOREIGN KEY(quote_id) REFERENCES quote(id)
-            )"""
-    cursor.execute(create_quote_table_query)
-    cursor.execute(create_author_table_query)
-    cursor.execute(create_quote_tag_table_query)
-
-
-def insert_to_quote_table(quote_id, quote, author):
-    cursor.execute("""INSERT INTO quote VALUES(?,?,?)""",(quote_id,quote,author))
-
-def insert_to_quote_tag(quote_id, tags_list):
-    for tag in tags_list:
-        cursor.execute('INSERT INTO quote_tag VALUES(?,?,?)',(None, tag,quote_id))
-
-def insert_to_author(name, born, reference):
-    cursor.execute("""INSERT INTO author VALUES (?,?, ?, ?)""",(None, name, born, reference))
-
-drop_tables()
-create_tables()
-
-for author_dict in quotes_data_dict['authors']:
-    name = author_dict['name']
-    born = author_dict['born']
-    reference =  author_dict['reference']
-    insert_to_author(name,born,reference)
-
-quote_id = 0
-for quote_dict in quotes_data_dict['quotes']:
-    quote = quote_dict['quote']
-    author = quote_dict['author']
-    tags_list = quote_dict['tags']
-    insert_to_quote_table(quote_id, quote, author)
-    insert_to_quote_tag(quote_id,tags_list)
-    quote_id += 1
-
+create_quote_table()            # function declaration starts at line 5
+create_author_table()           # function declaration starts at line 15
+create_quote_tag_table()        # function declaration starts at line 28
+insert_into_quote_table()       # function declaration starts at line 41
+insert_into_author_table()      # function declaration starts at line 49
+insert_into_quote_tag_table()   # function declaration starts at line 59
 
 connection.commit()
 connection.close
