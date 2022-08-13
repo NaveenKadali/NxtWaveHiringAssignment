659918b25f10631b56992e442a203c029daf5c3d
Naveen_Kadali <naveenkadali@outlook.com>
2022-08-13 16:57:14 +0530

    Re-named variable names and a new function Created get_author_dictionary_from_existing_authors function

--- crawl_quotes.py
+++ crawl_quotes.py
@@ -4,7 +4,7 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-# saves the quetes data (quotes_list and authors_list) into a json file
+# saves quotes_list and authors_list into a json file
 def save_to_json_file(quotes_list, authors_list):
     quotes_dictionary = {"quotes":quotes_list, 'authors':authors_list}
     json_data = json.dumps(quotes_dictionary,ensure_ascii=False ,indent=4)
@@ -12,9 +12,19 @@ def save_to_json_file(quotes_list, authors_list):
     file.write(json_data)
     file.close()
 
+# returns quote dictionaries list and author dictionaries list
+def get_quotes_list_and_authors_list(quotes_page_scraped_data):
+    quotes_list, authors_list = [],[]
+    for quote_dictionary, author_dictionary in quotes_page_scraped_data:
+        quotes_list.append(quote_dictionary)
+        if author_dictionary not in authors_list:
+            authors_list.append(author_dictionary)
+    return  (quotes_list,authors_list)
+
+
 # returns next page url if next page available else retuns None
-def get_next_page_url(soup):
-    pager_element = soup.find('ul',class_="pager")
+def get_next_page_url(quotes_page_parsed_response):
+    pager_element = quotes_page_parsed_response.find('ul',class_="pager") 
     next_page_list_element = pager_element.find('li', class_='next')
     if next_page_list_element != None:
         next_page_href = next_page_list_element.select_one('a', href=True)['href']
@@ -23,28 +33,47 @@ def get_next_page_url(soup):
     else:
         return None
 
-# returns a dictionary of author_name, born and reference url
-def get_author_dictionary(quote_container):
-    author_reference_url = get_author_reference_url(quote_container)
-    response = get_response(author_reference_url)
-    soup = parse_response_to_text_format(response)
-    author_name = soup.find('h3',class_='author-title').text.strip()
-    born_date = soup.find('span',class_='author-born-date').text.strip()
-    born_place = soup.find('span',class_='author-born-location').text.strip()
-    author_name = replace_speacial_chars_with_spaces(author_name)
-    born = born_date +" "+ born_place.strip()
-    reference = response.url
+# returns author dictionary if already exists in the scraped data
+def get_author_data_from_existing_authors(author_reference_url):
+    quotes_and_authors_list = get_quotes_list_and_authors_list(quotes_page_scraped_data)
+    authors_list = quotes_and_authors_list[1]
+    for author_dictionary in authors_list:
+        if author_dictionary['reference'] == author_reference_url:
+            return author_dictionary
+    else:
+        return None
+
+#returns a dictionary of author_name, born and reference url from reference page
+def get_author_dictionary_from_author_page(author_reference_url):
+    author_page_response = get_response(author_reference_url)
+    author_page_parsed_response = get_parsed_response(author_page_response)
+    author_name = author_page_parsed_response.find('h3',class_='author-title').text.strip()
+    born_date = author_page_parsed_response.find('span',class_='author-born-date').text.strip()
+    born_place = author_page_parsed_response.find('span',class_='author-born-location').text.strip()
+    author_name = replace_speacial_characters_with_space(author_name)
+    born = born_date +" "+ born_place.strip() 
+    reference = author_page_response.url
     author_dictionary = { 'name':author_name, 'born':born, 'reference':reference}
     return author_dictionary
 
-# returns the author reference urlof that quote
+
+# returns a dictionary of author_name, born and reference url
+def get_author_dictionary(author_reference_url):
+    author_dictionary = get_author_data_from_existing_authors(author_reference_url)
+    if author_dictionary is not None:
+        return author_dictionary
+    else:
+        author_dictionary = get_author_dictionary_from_author_page(author_reference_url)
+        return author_dictionary
+
+# returns the author reference url  that quote
 def get_author_reference_url(quote_container):
     author_href = quote_container.select_one('a')['href']
     author_reference_url = "http://quotes.toscrape.com"+author_href    
     return author_reference_url
 
 # removes special charachters and returns the plane text
-def replace_speacial_chars_with_spaces(text):
+def replace_speacial_characters_with_space(text):
     replaced_text = re.sub("[^\w\s]", " ", text)
     replaced_text = " ".join(replaced_text.split())
     return replaced_text
@@ -70,24 +99,25 @@ def get_quote_dictionary(quote_container):
     tags_container = quote_container.select('div .tag')
     tags_list = get_tags(tags_container)
     quote = remove_unicodes(quote)
-    author = replace_speacial_chars_with_spaces(author)
+    author = replace_speacial_characters_with_space(author)
     quote_dictionary = {"quote": quote, "author": author, "tags": tags_list}
     return quote_dictionary
 
-# web crawling starts by calling this function 
-def get_quotes_and_authors_data(soup):
+# returns a list of touples with quote_dictionary and author_dictionary
+def get_quotes_and_authors_data(quotes_page_parsed_response):
     quotes_and_authors_data = []
-    quote_containers = soup.find_all('div',class_= 'quote')
+    quote_containers = quotes_page_parsed_response.find_all('div',class_= 'quote')
     for quote_container in quote_containers:
         quote_dictionary = get_quote_dictionary(quote_container)
-        author_dictionary = get_author_dictionary(quote_container)
+        author_reference_url = get_author_reference_url(quote_container)
+        author_dictionary = get_author_dictionary(author_reference_url)
         quotes_and_authors_data.append((quote_dictionary, author_dictionary))
     return quotes_and_authors_data
     
-# returns a soup object by parseing the response object into text formart
-def parse_response_to_text_format(response):
-    soup = BeautifulSoup(response.content, "lxml")
-    return soup
+# returns parsed response for the given resposne object
+def get_parsed_response(response):
+    parsed_response = BeautifulSoup(response.content, "lxml")
+    return parsed_response
 
 # returns response object for the requested url
 def get_response(url): 
@@ -95,21 +125,17 @@ def get_response(url):
     return response
 
 #scrapping starts from this function
-def start_web_scraping():
-    quotes_list = []
-    authors_list = []
-    quotes_and_authors_data = []
-    url = "http://quotes.toscrape.com/"
-    while url != None:
-        response = get_response(url)
-        soup = parse_response_to_text_format(response) 
-        quotes_and_authors_data.extend(get_quotes_and_authors_data(soup))
-        url = get_next_page_url(soup)
-    for quote_dictionary, author_dictionary in quotes_and_authors_data:
-        quotes_list.append(quote_dictionary)
-        if author_dictionary not in authors_list:
-            authors_list.append(author_dictionary)
+def start_scraping_quotes_page():
+    global quotes_page_scraped_data
+    quotes_page_scraped_data = []
+    quotes_page_url = "http://quotes.toscrape.com/"
+    while quotes_page_url is not None:
+        quotes_page_response = get_response(quotes_page_url)
+        quotes_page_parsed_response = get_parsed_response(quotes_page_response) 
+        quotes_page_scraped_data.extend(get_quotes_and_authors_data(quotes_page_parsed_response))
+        quotes_page_url = get_next_page_url(quotes_page_parsed_response)
+    quotes_list, authors_list = get_quotes_list_and_authors_list(quotes_page_scraped_data)
     save_to_json_file(quotes_list, authors_list)
 
-start_web_scraping()
+start_scraping_quotes_page()
 print("Scraping completed successfully!")
