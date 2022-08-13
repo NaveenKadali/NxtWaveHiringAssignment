81b0150d41af1b2322873eeb9c2a3da03e842f9b
Naveen_Kadali <naveenkadali@outlook.com>
2022-08-13 17:01:50 +0530

    Added foriegn key relations to quote_tag table Modified Insert queries

--- store_quotes.py
+++ store_quotes.py
@@ -24,45 +24,49 @@ def insert_into_quote_tag_table(quote, tags_list):
 
 # inserts tag_id and tag into tag table
 def insert_into_tag_table(tags_list):
-    insert_query = "INSERT INTO tag VALUES(?,?)"
+    insert_query = "INSERT INTO tag(tag) VALUES(?)"
     for tag in tags_list:
-        execute_query(insert_query,None,tag)
+        execute_query(insert_query,tag)
 
 # inserts quote, author_id into quote table
 def insert_into_quote_table(quotes_list):
     author_ids = dict(execute_query("SELECT name, id FROM author;"))
-    insert_query = """INSERT INTO quote VALUES(?,?,?)"""
+    insert_query = """INSERT INTO quote(quote, author_id) VALUES(?,?)"""
     for quote_dictionary in quotes_list:
         quote, author_name = quote_dictionary["quote"], quote_dictionary["author"]
         author_id = author_ids[author_name]
-        execute_query(insert_query,None, quote, author_id)
+        execute_query(insert_query,  quote, author_id)
 
 # inserts author_data into author table
 def insert_into_author_table(authors_list):
-    insert_query = """INSERT INTO author VALUES (?,?, ?, ?)"""
+    insert_query = """INSERT INTO author(name,born,reference) VALUES (?, ?, ?)"""
     for author_dictionary in authors_list:
         author_name, author_born, reference_url = author_dictionary.values()
-        execute_query(insert_query,None, author_name, author_born, reference_url)
+        execute_query(insert_query,author_name, author_born, reference_url)
 
 # creates quote_tag juction table with quote_id and tag_id columns
 def create_quote_tag_table():
     create_quote_tag_junction_table_query = """CREATE TABLE quote_tag (
-        quote_id INTEGER NOT NULL, tag_id INTEGER NOT NULL
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
+        quote_id INTEGER NOT NULL, 
+        tag_id INTEGER NOT NULL,
+        FOREIGN KEY (quote_id) REFERENCES quote(id) ON DELETE CASCADE,
+        FOREIGN KEY (tag_id) REFERENCES tag(id) ON DELETE CASCADE
         ) """
     execute_query(create_quote_tag_junction_table_query)
 
 # creates tag table with id, tag columns
 def create_tag_table():
     create_tag_table_query = """CREATE TABLE tag (
-        id INTEGER PRIMARY KEY AUTOINCREMENT, 
-        tag VARCHAR(255)
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
+        tag VARCHAR(255) NOT NULL UNIQUE
         )""" 
     execute_query(create_tag_table_query)
 
 # creates quote table with id, quote, author_id columns
 def create_quote_table():
     create_quote_table_query = """CREATE TABLE quote (
-        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
         quote TEXT, 
         author_id INTEGER NOT NULL, 
         FOREIGN KEY(author_id) REFERENCES author(id) ON DELETE CASCADE
@@ -72,7 +76,7 @@ def create_quote_table():
 # creates author table with id, name, born and reference columns
 def create_author_table():
     create_author_table_query = """CREATE TABLE author (
-         id INTEGER PRIMARY KEY AUTOINCREMENT, 
+         id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 
          name VARCHAR(255), 
          born VARCHAR(255), 
          reference VARCHAR(255)
@@ -93,13 +97,13 @@ def get_all_unique_tags(quotes_list):
     return sorted(tags_list)
 
 # organizing data into quotee.db starts from this function
-def organize_data_into_db():
+def organize_quotes_data_into_db():
     authors_list = quotes_data["authors"]
     quotes_list = quotes_data["quotes"]
     tags_list = get_all_unique_tags(quotes_list)
     delete_existing_tables("author","quote","tag","quote_tag")
-    create_quote_table()
     create_author_table()
+    create_quote_table()
     create_tag_table()
     create_quote_tag_table()
     insert_into_author_table(authors_list) 
@@ -109,7 +113,7 @@ def organize_data_into_db():
         quote, tags_list = quote_dictionary["quote"], quote_dictionary["tags"]
         insert_into_quote_tag_table(quote,tags_list)
 
-organize_data_into_db()
+organize_quotes_data_into_db()
 connection.commit()
 connection.close
  
